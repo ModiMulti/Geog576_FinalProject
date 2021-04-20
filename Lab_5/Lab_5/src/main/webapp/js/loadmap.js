32b9c4ca5e5ed72f23bd76c93f0c0aeba509d2c9
ModiMulti <54697416+ModiMulti@users.noreply.github.com>
2021-04-19 21:14:24 -0400

    initial commit

---
+++ Lab_5/Lab_5/src/main/webapp/js/loadmap.js
@@ -0,0 +1,141 @@
+var map;
+var place;
+var autocomplete;
+var infowindow = new google.maps.InfoWindow();
+
+function initialization() {
+    showAllReports();
+    initAutocomplete();
+}
+
+function showAllReports() {
+    $.ajax({
+        url: 'RunQuery.jsp',
+        type: 'POST',
+        data: { "tab_id": "1"},
+        success: function(reports) {
+            mapInitialization(reports);
+        },
+        error: function(xhr, status, error) {
+            alert("An AJAX error occured: " + status + "\nError: " + error);
+        }
+    });
+}
+
+function mapInitialization(reports) {
+    var mapOptions = {
+        mapTypeId : google.maps.MapTypeId.ROADMAP, // Set the type of Map
+    };
+
+    // Render the map within the empty div
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+    var bounds = new google.maps.LatLngBounds ();
+
+    $.each(reports, function(i, e) {
+        var long = Number(e['longitude']);
+        var lat = Number(e['latitude']);
+        var latlng = new google.maps.LatLng(lat, long);
+
+        bounds.extend(latlng);
+
+        // Create the infoWindow content
+        var contentStr = '<h4>Report Details</h4><hr>';
+        contentStr += '<p><b>' + 'Disaster' + ':</b>&nbsp' + e['disaster'] + '</p>';
+        contentStr += '<p><b>' + 'Report Type' + ':</b>&nbsp' + e['report_type'] +
+            '</p>';
+        if (e['report_type'] == 'request' || e['report_type'] == 'donation') {
+            contentStr += '<p><b>' + 'Resource Type' + ':</b>&nbsp' +
+                e['resource_type'] + '</p>';
+        }
+        else if (e['report_type'] == 'damage') {
+            contentStr += '<p><b>' + 'Damage Type' + ':</b>&nbsp' + e['damage_type']
+                + '</p>';
+        }
+
+        // Answer - Question 1 - Lab 6
+        contentStr += '<p><b>' + 'Reportor' + ':</b>&nbsp' + e['first_name'] + '&nbsp' + e['last_name'] + '</p>';
+
+        contentStr += '<p><b>' + 'Timestamp' + ':</b>&nbsp' +
+            e['time_stamp'].substring(0,19) + '</p>';
+        if ('message' in e){
+            contentStr += '<p><b>' + 'Message' + ':</b>&nbsp' + e['message'] + '</p>';
+        }
+
+        // Answer - Question 2 - Lab 6
+        var icon_img = '';
+
+        if(e['report_type'] == 'donation') {
+            icon_img = 'http://maps.google.com/mapfiles/kml/shapes/convenience.png';
+        }else if(e['report_type'] == 'request'){
+            icon_img = 'http://maps.google.com/mapfiles/kml/shapes/info.png';
+        }else if(e['report_type'] == 'damage'){
+            icon_img = 'http://maps.google.com/mapfiles/kml/shapes/caution.png';
+        }
+
+        var icon = {
+            url: icon_img, // url
+            scaledSize: new google.maps.Size(30, 20), // scaled size
+            origin: new google.maps.Point(0, 0), // origin
+            anchor: new google.maps.Point(0, 0) // anchor
+        };
+
+        // Create the marker
+        var marker = new google.maps.Marker({ // Set the marker
+            position : latlng, // Position marker to coordinates
+            map : map, // assign the market to our map variable
+            icon: icon_img,
+            customInfo: contentStr
+        });
+
+        // Add a Click Listener to the marker
+        google.maps.event.addListener(marker, 'click', function() {
+            // use 'customInfo' to customize infoWindow
+            infowindow.setContent(marker['customInfo']);
+            infowindow.open(map, marker); // Open InfoWindow
+        });
+
+    });
+
+    map.fitBounds (bounds);
+
+}
+
+function initAutocomplete() {
+    // Create the autocomplete object
+    autocomplete = new google.maps.places.Autocomplete(document.
+    getElementById('autocomplete'));
+
+    // When the user selects an address from the dropdown, show the place selected
+    autocomplete.addListener('place_changed', onPlaceChanged);
+}
+
+//Answer - Question 3 - Lab 6
+function onPlaceChanged() {
+    infowindow.close();
+    place = autocomplete.getPlace();
+
+    if (!place.geometry || !place.geometry.location) {
+        // User entered the name of a Place that was not suggested and
+        // pressed the Enter key, or the Place Details request failed.
+        window.alert("No details available for input: '" + place.name + "'");
+        return;
+    }
+
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+    } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);
+    }
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+    infowindowContent.children["place-name"].textContent = place.name;
+    infowindowContent.children["place-address"].textContent =
+        place.formatted_address;
+    infowindow.open(map, marker);
+}
+
+//Execute our 'initialization' function once the page has loaded.
+google.maps.event.addDomListener(window, 'load', initialization);
